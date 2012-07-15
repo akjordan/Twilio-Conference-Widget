cb84835e3088cab06c703071e5a7895e8e349c5c
Andrew <andrew.k.jordan@gmail.com>
2012-07-14 17:31:37 -0700

    simple auth fixed

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == twilio, client]
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ENV['RACK_USER'], ENV['RACK_PW']]
   end
 
 end
